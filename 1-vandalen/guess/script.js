84f1079f7effb4c351a8fdcb7e13c526eab9405b
fl222hr <fl222hr@student.lnu.se>
2014-11-19 22:29:42 +0000

    completed guess

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,7 +2,7 @@
 
 window.onload = function(){
 
-	var secret = Math.floor( Math.random() * 100)+1; // Slumpa ett tal mellan 0-100.
+	var secret = Math.floor( Math.random() * 100)+1; // Slumpa ett tal mellan 1-100.
 	var guesses = 0; // tal för att hålla koll på antalet gissningar
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
@@ -11,30 +11,24 @@ window.onload = function(){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 		
-		if(number < 0 || number >100){
+		if(number < 1 || number >100){
 		guesses--;//om det inmatade inte är ett giltigt tal bör det inte räknas som en gissning
-		return	[false, "Talet är utanför intervallet 0 - 100"]	;
+		return	[false, "Talet är utanför intervallet 1 - 100"]	;
 		}
-		if(number > secret){
-		return [false, "Det hemliga talet är lägre!"];
-		}
-		if(number < secret){
-		return	[false, "Det hemliga talet är högre!"];
-		}
-		if(number == secret){
-		return [true, "Grattis du vann! Det hemliga talet var "+secret+" och du behövde "+guesses+" gissningar för att hitta det."];
-		}
-		else{
-			guesses--; //om det inmatade inte är ett tal bör det inte räknas som en gissning
-			return	[false, "Inte ett tal mellan 0-100"];
-		}
+			if(number > secret){
+			return [false, "Det hemliga talet är lägre!"];
+			}
+				if(number < secret){
+				return	[false, "Det hemliga talet är högre!"];
+							}
+					if(number == secret){
+					return [true, "Grattis du vann! Det hemliga talet var "+secret+" och du behövde "+guesses+" gissningar för att hitta det."];
+					}
+						else{
+							guesses--; //om det inmatade inte är ett tal bör det inte räknas som en gissning
+							return	[false, "Inte ett tal mellan 1-100"];
+						}
 
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
 	};
 	
 	// ------------------------------------------------------------------------------

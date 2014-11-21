4deb55205cadb7f0c7031186a12fe7e5845f76f1
fl222hr <fl222hr@student.lnu.se>
2014-11-21 08:46:19 +0000

    added some code to vandalen

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,34 @@
 "use strict";
 
 var makePerson = function(persArr){
+  
+    console.log("Hej");
+    var theNames = new Array();
+    var theMaxAge = 0;
+    var theMinAge = 1000;
+    var theAverageAge = 0;
+    var nameString = "";
 
-
+  for(var i = 0; i < persArr.length; i++){
+    if(persArr[i].age > theMaxAge) theMaxAge = persArr[i].age;
+    if(persArr[i].age < theMinAge) theMinAge = persArr[i].age;
+    theAverageAge = theAverageAge + persArr[i].age;
+    theNames.push(persArr[i].name);
+  }
+    
+    theNames.sort();
+      for(var i = 0; i < theNames.length; i++){
+    nameString = nameString + theNames[i] + ",";
+  }
+    
+    var result = {
+        minAge: theMinAge, 
+        maxAge: theMaxAge, 
+        averageAge: theAverageAge,
+        names: nameString
+    };
+    
+    return result;
 	// Din kod här...
-
 }
 

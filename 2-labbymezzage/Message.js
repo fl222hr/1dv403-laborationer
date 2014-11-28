6ce88b745a158ea0e16fd7ad7e5629f55b6d5ba9
fl222hr <fl222hr@student.lnu.se>
2014-11-28 08:26:30 +0000

    cleaned up Message

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -3,26 +3,26 @@ function Message(message, date){
     
     this.getText = function(){
         return message;
-    }
+    };
     
     this.setText = function(text){
         message = text;
-    }
+    };
     
     this.getDate = function(){
         return date;
-    }
+    };
     
     this.setDate = function(newDate){
         date = newDate;
-    }
+    };
 }
 
 Message.prototype.toString = function(){
     return this.getText() + " (" + this.getDate() + ") ";
-}
+};
 
 Message.prototype.getHTMLText = function(){
     return this.getText().replace(/\n/g, "<br />");  
-}
+};
 
